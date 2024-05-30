92deca8a0c95c1579e4689743d4ab0ae0d3b7ab9
Breno de Carvalho Correia <brenodecarvalhocorreia@gmail.com>
2024-05-30 12:51:43 -0300

    feat: removing geoloc scripts from homepage

--- bot_map_app/lib/home_page.dart
+++ bot_map_app/lib/home_page.dart
@@ -1,41 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-
-Future<Position> _posicaoAtual() async {
-  LocationPermission permissao;
-  bool ativado = await Geolocator.isLocationServiceEnabled();
-
-  if (!ativado) {
-    return Future.error("Habilite a localização");
-  }
-
-  permissao = await Geolocator.checkPermission();
-  if (permissao == LocationPermission.denied) {
-    permissao = await Geolocator.requestPermission();
-    if (permissao == LocationPermission.denied) {
-      return Future.error("Permissão negada");
-    }
-  }
-
-  if (permissao == LocationPermission.deniedForever) {
-    return Future.error("Permissão negada permanentemente");
-  }
-
-  return await Geolocator.getCurrentPosition();
-}
-
-void getLocalization() async {
-  try {
-    Position posicao = await _posicaoAtual();
-    double lat = posicao.latitude;
-    double long = posicao.longitude;
-
-    debugPrint("Latitude: $lat, Longitude: $long");
-  } catch (e) {
-    debugPrint("Erro: $e");
-  }
-  
-}
+import 'package:bot_map/geoposition.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -50,7 +14,7 @@ class HomePage extends StatelessWidget {
           const Text("Oiiiiii"),
           ElevatedButton(
             onPressed: () {
-              getLocalization();
+              getLocal();
             },
             child: const Icon(Icons.play_arrow_outlined),
           ),
