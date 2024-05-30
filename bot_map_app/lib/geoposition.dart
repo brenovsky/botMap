15c717bd9aa7517de2dfa75f2eb6dd5ab1cf85d4
Breno de Carvalho Correia <brenodecarvalhocorreia@gmail.com>
2024-05-30 12:51:06 -0300

    add: geoposition scripts

---
+++ bot_map_app/lib/geoposition.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+
+Future<Position> _posicaoAtual() async {
+  LocationPermission permissao;
+  bool ativado = await Geolocator.isLocationServiceEnabled();
+
+  if (!ativado) {
+    return Future.error("Habilite a localização");
+  }
+
+  permissao = await Geolocator.checkPermission();
+  if (permissao == LocationPermission.denied) {
+    permissao = await Geolocator.requestPermission();
+    if (permissao == LocationPermission.denied) {
+      return Future.error("Permissão negada");
+    }
+  }
+
+  if (permissao == LocationPermission.deniedForever) {
+    return Future.error("Permissão negada permanentemente");
+  }
+
+  return await Geolocator.getCurrentPosition();
+}
+
+void getLocal() async {
+  try {
+    Position posicao = await _posicaoAtual();
+    double lat = posicao.latitude;
+    double long = posicao.longitude;
+
+    List<Placemark> placemarks = await placemarkFromCoordinates(lat, long);
+
+    debugPrint(formatPlaceMark(placemarks[0]));
+    
+  } catch (e) {
+    debugPrint("Erro: $e");
+  }
+}
+
+String formatPlaceMark(Placemark placemarks) {
+  return "${placemarks.street}, ${placemarks.name} - ${placemarks.subLocality}, ${placemarks.subAdministrativeArea}";
+}
