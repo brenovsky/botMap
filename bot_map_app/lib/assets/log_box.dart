cc783c71125cd2382068a2753aa047fdb37c6a18
Breno de Carvalho Correia <brenodecarvalhocorreia@gmail.com>
2024-05-31 14:18:07 -0300

    feat(assets/logbox): listview

--- bot_map_app/lib/assets/log_box.dart
+++ bot_map_app/lib/assets/log_box.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
-void printLog() {
-  
-}
+void printLog() {}
 
 class LogBox extends StatelessWidget {
   const LogBox({super.key});
@@ -10,11 +10,17 @@ class LogBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      width: 400,
-      color: Colors.red,
-      child:
-        const Text("Mensagem Enviada")
-    );
+        height: 300,
+        width: 400,
+        decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 2)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            
+            children: const [
+              Text("Mensagem enviada: Tv. Malafaia, 448 - Colubandê, São Goncaço", style: TextStyle(fontSize: 16),),
+            ],
+          ),
+        ));
   }
 }
